5d4f9dc93bd1c6e3ae8d73bf04c4cf326b4e6c05
saalini-t <saalini.t@gmail.com>
2026-02-24 16:19:06 +0530

    UC1: Add welcome message

--- src/com/saalini/palindrome/PalindromeCheckerApp.java
+++ src/com/saalini/palindrome/PalindromeCheckerApp.java
@@ -4,7 +4,9 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Palindrome Checker App Started");
+        System.out.println("PALINDROME CHECKER APP");
+        System.out.println("Version 1.0");
+        System.out.println("Welcome to the Application");
 
     }
 }
