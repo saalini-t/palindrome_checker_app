aff7c2a3ce11f770ced71404eb0549417b5a023b
saalini-t <saalini.t@gmail.com>
2026-02-24 16:21:25 +0530

    UC2: Hardcoded palindrome

--- src/com/saalini/palindrome/PalindromeCheckerApp.java
+++ src/com/saalini/palindrome/PalindromeCheckerApp.java
@@ -5,8 +5,18 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         System.out.println("PALINDROME CHECKER APP");
-        System.out.println("Version 1.0");
-        System.out.println("Welcome to the Application");
 
+        String word = "madam";
+        String reversed = "";
+
+        for(int i = word.length()-1; i >= 0; i--){
+            reversed += word.charAt(i);
+        }
+
+        if(word.equals(reversed)){
+            System.out.println(word + " is Palindrome");
+        }else{
+            System.out.println(word + " is NOT Palindrome");
+        }
     }
 }
